4536ae34ae4343ea334e2a6702d2c022d2a4ad4c
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-30 16:49:38 +0530

    Edit Contact Details

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -9,44 +9,70 @@ namespace AddressBook
         {
             Console.WriteLine("Welcome to Address Book");
 
-            Console.WriteLine("\nEnter First Name: ");
-            string? firstName = Console.ReadLine();
-
-            Console.WriteLine("\nEnter Last Name: ");
-            string? lastName = Console.ReadLine();
-
-            Console.WriteLine("\nEnter Mobile Number: ");
-            long mobileNumber = Convert.ToInt64(Console.ReadLine());
-
-            Console.WriteLine("\nEnter EMail: ");
-            string? eMail = Console.ReadLine();
-
-            Console.WriteLine("\nEnter Address: ");
-            string? address = Console.ReadLine();
-
-            Console.WriteLine("\nEnter City: ");
-            string? city = Console.ReadLine();
-
-            Console.WriteLine("\nEnter State: ");
-            string? state = Console.ReadLine();
-
-            Console.WriteLine("\nEnter Zip: ");
-            int zip = Convert.ToInt32(Console.ReadLine());
-
-            ContactDetails contactDetail = new ContactDetails()
-            {
-                FirstName = firstName,
-                LastName = lastName,
-                MobileNumber = mobileNumber,
-                EMail = eMail,
-                Address = address,
-                City = city,
-                State = state,
-                Zip = zip
-            };
-
             ContactDetailsRepository contactDetails = new ContactDetailsRepository();
-            contactDetails.AddContactDetails(contactDetail);
+            Console.WriteLine("\nEnter X to Add Contact Details.");
+            string? input = Console.ReadLine();
+
+            ContactDetails contactDetail = null;
+
+            while (input == "X")
+            {
+                Console.WriteLine("\nEnter First Name: ");
+                string? firstName = Console.ReadLine();
+
+                Console.WriteLine("\nEnter Last Name: ");
+                string? lastName = Console.ReadLine();
+
+                Console.WriteLine("\nEnter Mobile Number: ");
+                long mobileNumber = Convert.ToInt64(Console.ReadLine());
+
+                Console.WriteLine("\nEnter EMail: ");
+                string? eMail = Console.ReadLine();
+
+                Console.WriteLine("\nEnter Address: ");
+                string? address = Console.ReadLine();
+
+                Console.WriteLine("\nEnter City: ");
+                string? city = Console.ReadLine();
+
+                Console.WriteLine("\nEnter State: ");
+                string? state = Console.ReadLine();
+
+                Console.WriteLine("\nEnter Zip: ");
+                int zip = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\nEnter X to Add Contact Details or enter E to exit.");
+                input = Console.ReadLine();
+
+                contactDetail = new ContactDetails()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    MobileNumber = mobileNumber,
+                    EMail = eMail,
+                    Address = address,
+                    City = city,
+                    State = state,
+                    Zip = zip
+                };
+                contactDetails.AddContactDetails(contactDetail);
+                contactDetails.DisplayContact();
+            }
+
+            Console.WriteLine("\nEnter Y to edit Contact Details");
+            string? input2 = Console.ReadLine();
+
+            while (input2 == "Y")
+            {
+                Console.WriteLine("\nEnter Mobile Number");
+                contactDetails.EditContactDetails(Convert.ToInt64(Console.ReadLine()));
+                contactDetails.DisplayContact();
+
+                Console.WriteLine("\nEnter Y to edit Contact Details or enter E to exit");
+                input2 = Console.ReadLine();
+
+            }
+
             contactDetails.DisplayContact();
             Console.ReadLine();
         }
--- AddressBook/Repository/ContactDetailsRepository.cs
+++ AddressBook/Repository/ContactDetailsRepository.cs
@@ -16,6 +16,65 @@ namespace AddressBook.Repository
             contactDetails.Add(contactDetail.MobileNumber, contactDetail);
         }
 
+        public void EditContactDetails(long mobileNumber)
+        {
+            var obj = contactDetails[mobileNumber];
+            contactDetails.Remove(mobileNumber);
+            Console.WriteLine("\nSelect from below to edit (Enter number 1-8) \n1.First Name \n2.Last Name \n3.Mobile Number \n4.EMail \n5.Address \n6.City \n7.State \n8.Zip");
+            int edit = Convert.ToInt32(Console.ReadLine());
+            switch (edit)
+            {
+                case 1:
+                    Console.WriteLine("\nEnter New First Name");
+                    string firstName = Console.ReadLine();
+                    obj.FirstName = firstName;
+                    contactDetails.Add(obj.MobileNumber, obj);
+                    break;
+                case 2:
+                    Console.WriteLine("\nEnter New Last Name");
+                    string lastName = Console.ReadLine();
+                    obj.LastName = lastName;
+                    contactDetails.Add(obj.MobileNumber, obj);
+                    break;
+                case 3:
+                    Console.WriteLine("\nEnter New Mobile Number");
+                    mobileNumber = Convert.ToInt64(Console.ReadLine());
+                    obj.MobileNumber = mobileNumber;
+                    contactDetails.Add(obj.MobileNumber, obj);
+                    break;
+                case 4:
+                    Console.WriteLine("\nEnter New EMail");
+                    string email = Console.ReadLine();
+                    obj.EMail = email;
+                    contactDetails.Add(obj.MobileNumber, obj);
+                    break;
+                case 5:
+                    Console.WriteLine("\nEnter New Address");
+                    string address = Console.ReadLine();
+                    obj.Address = address;
+                    contactDetails.Add(obj.MobileNumber, obj);
+                    break;
+                case 6:
+                    Console.WriteLine("\nEnter New City");
+                    string city = Console.ReadLine();
+                    obj.City = city;
+                    contactDetails.Add(obj.MobileNumber, obj);
+                    break;
+                case 7:
+                    Console.WriteLine("\nEnter New State");
+                    string state = Console.ReadLine();
+                    obj.State = state;
+                    contactDetails.Add(obj.MobileNumber, obj);
+                    break;
+                case 8:
+                    Console.WriteLine("\nEnter New Zip");
+                    int zip = Convert.ToInt32(Console.ReadLine());
+                    obj.Zip = zip;
+                    contactDetails.Add(obj.MobileNumber, obj);
+                    break;
+            }
+        }
+
         public void DisplayContact()
         {
             foreach (var item in contactDetails)
